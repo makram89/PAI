c67d94c62e7d5459a7c00c916cb978b147f7c142
makram89 <marasss.kamin@gmail.com>
2024-12-22 20:26:37 +0100

    k8 init + react

---
+++ k8-config/cluster/create-cluster.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+minikube --driver docker \
+    --nodes 1 \
+    --cpus=no-limit --memory=no-limit \
+    --kubernetes-version=v1.30.0-rc.2 \
+    --container-runtime=docker \
+    --profile=multi-app \
+    start
+
+
+minikube profile multi-app
+
+kubectl config set-context --current --namespace multi-app
+
+# Sometimes need to be run
+minikube tunnel
